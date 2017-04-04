ec52eed48c372b5d7e2bb2cfa990430d4f0e2639
Marcel Jepma <m.jepma@jispro.nl>
2017-04-05 00:16:12 +0200

    Added demo script

--- scripts/run-demo.sh
+++ scripts/run-demo.sh
@@ -3,9 +3,14 @@
 if [ "$(kubectl -n sock-shop get  deployments --selector=name=front-end -o jsonpath='{.items[*].metadata.labels.version}')" == "2.0.0" ]; then
 
     echo "Switch to 1.0.0"
+    kubectl replace -f /opt/demo-files/microservices-demo/manifests-start/front-end-dep.yaml
 
 else
 
     echo "Switch to 2.0.0"
+    kubectl replace -f /opt/demo-files/microservices-demo/manifests-update/front-end-dep.yaml
 
 fi
+
+echo "Now we sleep for 2 minutes..."
+sleep 120
